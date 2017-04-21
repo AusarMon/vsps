01b40dc4da43d1dfe5531ffce99d948da39ff689
AusarMon <zxc294811099@qq.com>
2017-04-21 22:19:55 +0800

    添加 CORS 支持

--- vsps/src/main/java/com/scut/vsp/config/security/SecurityConfig.java
+++ vsps/src/main/java/com/scut/vsp/config/security/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/cors", config);
+        source.registerCorsConfiguration("*", config);
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
         bean.setOrder(0);
         return bean;
