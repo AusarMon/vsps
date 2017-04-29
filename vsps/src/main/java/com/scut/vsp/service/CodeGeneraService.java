b7c9e4402017542bb81d6ac02c3e1fb1e29251ed
fishersosoo <fishersosoo@outlook.com>
2017-04-29 14:25:34 +0800

    fix

--- vsps/src/main/java/com/scut/vsp/service/CodeGeneraService.java
+++ vsps/src/main/java/com/scut/vsp/service/CodeGeneraService.java
@@ -2,6 +2,8 @@ package com.scut.vsp.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 /**
  * Created by sosoo on 2017/2/27.
  */
@@ -16,7 +18,7 @@ public class CodeGeneraService {
             throw new NullPointerException("No CodeGeneraServiceProviderInterface");
         return serviceProvider.generateCode(jsonString);
     }
-    public String generateTestCode(String jsonString) throws NullPointerException {
+    public String generateTestCode(String jsonString) throws NullPointerException, IOException {
         if (serviceProvider == null)
             throw new NullPointerException("No CodeGeneraServiceProviderInterface");
         return serviceProvider.generateTestCode(jsonString);
